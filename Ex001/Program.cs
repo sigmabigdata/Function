cc7d482c9f5188fcc6373f6cb26bce5cb15b6792
sigmabigdata <sigmabigdata@gmail.com>
2023-01-08 21:41:31 +0300

    Функции

---
+++ Ex001/Program.cs
@@ -0,0 +1,140 @@
+﻿// Ничего не принимают и ничего не возыращают
+/*void Method1()
+{
+    Console.WriteLine("Автор.....");
+}
+
+Method1(); */
+
+// Не возвращают, но принимают
+
+/*void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+Method2(msg: "Текст сообщения");*/
+
+/*void Method21(string msg, int count)
+{
+    int i = 0;
+    while(i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+
+Method21(msg: "Текст", count: 4);*/
+
+//Вид3
+/*int Method3()
+{
+    return DateTime.Now.Year;
+}
+int Year = Method3();
+Console.WriteLine(Year);*/
+
+/*string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while(i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "wsgsdvsvsd ");
+Console.WriteLine(res);*/
+
+
+
+/*string Method4(int count, string text)
+{
+    string result = String.Empty;
+
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+        
+    }
+    return result;
+}
+
+string res = Method4(10, "wsgsdvsvsd ");
+Console.WriteLine(res);*/
+
+
+/*for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }      
+    Console.WriteLine();*/
+
+/*string text = " - Я думаю, - сказал князь, улыбаясь, - что, "
+              + "ежели бы вас послали вместо нашего милого Винценгероде,"
+              + " вы бы взяли приступом согласие прусского короля. "
+              + " Вы так заботливы. Вы дадите мне чаю?";
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+  
+    return result;
+}
+    string NewText = Replace(text,' ','|');
+    Console.WriteLine(NewText);
+    Console.WriteLine();
+    NewText = Replace(NewText, 'к', 'К');
+    Console.WriteLine(NewText);
+
+    NewText = Replace(NewText, 'с', 'С');
+    Console.WriteLine(NewText);*/
+
+
+    int[] arr = {1, 5, 87, 5, 85, 7, 5, 5, 87, 8, 6, 9, 2, 54, 4};
+
+    void PrintArray(int[] array)
+    {
+        int count = array.Length;
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+        Console.WriteLine();
+    }
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j; 
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+ }
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
+
+
+
+
