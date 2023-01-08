306e476c4c1464e2d22a56ee3e9380afd78b6357
sigmabigdata <sigmabigdata@gmail.com>
2023-01-08 22:58:35 +0300

    Функции

---
+++ Ext002/Program.cs
@@ -0,0 +1,38 @@
+﻿// Определить массив
+//Найти минимальное значение
+//Отсортировать попорядку
+
+int[] array = {1, 65, 5, 2, 1, 651, 65, 654, 6, 84, 874, 1574, 321};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    int countt = array.Length -1;
+    for (int i = 0; i < countt; i++)
+    {
+        int minPosition = i;
+        int count = array.Length;
+        for (int j = i + 1; j < count; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j; 
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+ }
+
+
+
+PrintArray(array);
+SelectionSort(array);
+PrintArray(array);
